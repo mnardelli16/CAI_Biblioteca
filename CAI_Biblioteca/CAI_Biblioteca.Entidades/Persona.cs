e76f5c22f70610b4f218cfd50c20cd00b3b1d720
martinbermudez <70667459+martinbermudez@users.noreply.github.com>
2020-11-04 20:12:04 -0300

    PruebaV1
    V1

---
+++ CAI_Biblioteca/CAI_Biblioteca.Entidades/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Biblioteca.Entidades
+{
+    class Persona
+    {
+        private int id;
+        private string name;
+
+        public Persona (int ID, string Name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
